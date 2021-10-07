dd9053ca7314fe8892d207fbd7e152efec9c03db
weifuquan <weifuquan>
2021-10-07 15:19:56 +0800

    修改输出

--- main/java/HelloWorld.java
+++ main/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("我的github项目");
-        System.out.println("大家都觉得");
+        System.out.println("大家都觉得可以");
 
     }
 }
